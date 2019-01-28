72f49dc8d83b50121030141d8286f4330621a106
Alexander Bobrov <al.bobrov.mail@gmail.com>
2019-01-28 22:28:07 +0500

    Added password and username to /admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :http_authenticate
 
+  def http_authenticate
+    # return true unless Rails.env == 'production'
+
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'secret' && password == 'password'
+    end
+  end
 end
